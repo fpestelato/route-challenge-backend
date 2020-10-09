c2be234fbd9c111dd455c60e33e4e397094c21da
fpestelato <fpestelato@gmail.com>
2020-10-09 03:16:23 -0300

    Added port to config file

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const config = () => ({
   database: {
     uri: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`,
-  }
+  },
 });
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
-  console.log(`Application is running on: ${await app.getUrl()}`);
+  const configService = app.get(ConfigService);
+  const port =  configService.get('PORT');
+  await app.listen(port);
+  console.log(`Application is running on: ${port}`);
 }
 bootstrap();
